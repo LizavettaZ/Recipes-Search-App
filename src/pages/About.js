1ff693076633349d6bfa6bfbe942a215595215ed
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-14 15:10:04 +0300

    create About

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import classes from '../style/pages/About.module.scss'
+import Food from '../img/about/Group 32.png'
+import SearchBar from '../img/about/search bar 1.png'
+import f from '../img/about/bg search.png'
+
+
+const About = () => {
+  return (
+    <div className={classes.About}>
+      <h3>How it works</h3>
+      <div className={classes.about__step}>
+        <div className={classes.step__img}>
+          <img src={Food} alt="Food"/>
+        </div>
+          <p className={classes.step__text}>Enter your products</p>
+      </div>
+      <div className={classes.about__step}>
+        <p className={classes.step__text}>Search for recipes</p>
+        <div className={classes.step__img}>
+          <img src={SearchBar} alt="Search bar"/>
+        </div>
+      </div>
+      <div className={classes.about__step_last}>
+        <p>Get Recommended Recipes</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
