6378b082776090a1e3cbcfeb2ff1869bffb1b41c
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-14 15:10:48 +0300

    create Loader

---
+++ src/components/UI/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from '../../style/components/Loader.module.scss'
+import Logo from '../../img/Logo.png'
+
+
+const Loader = () => {
+  return (
+    <div className={classes.Loader_container}>
+      <img src={Logo} alt = "Logo"/>
+      <div className={classes.Loader}>
+        <div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
