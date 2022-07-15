1668a5028be5371f122900351a205719d9e99385
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-15 11:37:23 +0300

    add new reducers

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux'
+import { alertReducer } from './alert'
+import { searchReducer } from './search'
+import { layoutReducer } from './layout';
 
 
 export default combineReducers({
-
+  alert: alertReducer,
+  search: searchReducer,
+  layout: layoutReducer
 })
