06c12aa2aed6ddf565f4a8a5fea0ce18f6c688ea
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-15 18:13:02 +0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import classes from './style/App.module.scss'
 import Layout from './hoc/Layout'
+import Home from './pages/Home'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Search from './pages/Search'
+import Recipe from './pages/Recipe'
 
 
 function App() {
   return (
     <Layout>
-      <div className={classes.App}>
-        It's working!
-      </div>
+      <Routes>
+        <Route path='/' exact="true" element={<Home/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/recipe/:label' element={<Recipe/>}/>
+        <Route path='/redirect' element={<Navigate to='/' />}/>
+      </Routes>
     </Layout>
-
   )
 }
 
