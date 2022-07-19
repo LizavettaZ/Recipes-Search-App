ab47794e6aff921a25ef7eabf316f2080aeba221
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-19 10:50:45 +0300

    create My Recipe page

---
+++ src/pages/MyRecipes.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import classes from '../style/pages/MyRecipes.module.scss'
+import Loader from '../components/UI/Loader'
+import RecipeCard from '../components/RecipeCard'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeBg } from '../store/actions/layout'
+
+const MyRecipes = () => {
+  const loading = useSelector(state => state.search.loading)
+  const recipes = useSelector(state => state.search.recipes)
+  const dispatch = useDispatch()
+
+  useEffect(() => {dispatch(changeBg('myRecipes'))}, [])
+
+  return (
+    <div className={classes.MyRecipes}>
+      <h2>My Recipes...</h2>
+      { loading
+        ? <Loader/>
+        : <div className={classes.my_recipes__list}>
+          {recipes.map((recipe, index) => (
+            <div className = {classes.my_recipe} key = {Date.now() + index}>
+              <RecipeCard
+                recipe = {recipe.recipe}
+              />
+            </div>
+          )) }
+        </div>
+      }
+    </div>
+  )
+}
+
+export default MyRecipes
