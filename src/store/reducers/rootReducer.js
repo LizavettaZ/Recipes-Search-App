962a207f14ce7789c3d9179ea2754b73019f3d42
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-19 17:55:46 +0300

    added reducers

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
 import { alertReducer } from './alert'
 import { searchReducer } from './search'
-import { layoutReducer } from './layout';
+import { layoutReducer } from './layout'
+import { authReducer } from './auth'
+import { favoriteReducer } from './favorite'
 
 
 export default combineReducers({
   alert: alertReducer,
   search: searchReducer,
-  layout: layoutReducer
+  layout: layoutReducer,
+  auth: authReducer,
+  favorite: favoriteReducer
 })
