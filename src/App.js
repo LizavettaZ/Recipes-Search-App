39e1d842646cd632b19a5b3f688e71bd126e018a
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-21 16:21:23 +0300

    added autologin & fix route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import classes from './style/App.module.scss'
 import Layout from './hoc/Layout'
 import Home from './pages/Home'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Search from './pages/Search'
 import Recipe from './pages/Recipe'
 import MyRecipes from './pages/MyRecipes'
 import Auth from './pages/Auth'
+import { useDispatch, useSelector } from 'react-redux'
+import NotFound from './pages/NotFound'
+import { useEffect } from 'react'
+import { authLogin } from './store/actions/auth'
 
 
 function App() {
+  const isAuthenticated = useSelector(state => !!state.auth.token)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+     dispatch(authLogin())
+  })
+
   return (
     <Layout>
       <Routes>
@@ -17,9 +28,9 @@ function App() {
         <Route path='/search' element={<Search/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/recipe/:label' element={<Recipe/>}/>
-        <Route path='/my_recipes' element={<MyRecipes/>}/>
+        {isAuthenticated && <Route path='/my_recipes' element={<MyRecipes/>}/>}
         <Route path='/auth' element={<Auth/>}/>
-        <Route path='/redirect' element={<Navigate to='/' />}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Layout>
   )
