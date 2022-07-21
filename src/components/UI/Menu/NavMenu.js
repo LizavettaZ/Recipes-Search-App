5e6db0071e522cdc5e2d16d96699d894b2fccf74
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-21 16:23:24 +0300

    fixed links & added logout

--- src/components/UI/Menu/NavMenu.js
+++ src/components/UI/Menu/NavMenu.js
@@ -2,46 +2,64 @@ import React, { Fragment, memo, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import BurgerMenu from './BurgerMenu'
 import classes from '../../../style/components/Menu.module.scss'
-import Logo from '../../../img/Logo.png'
-import User from '../../../img/icons/user.png'
+import Logo from '../../../img/Logo.svg'
+import User from '../../../img/icons/user.svg'
+import { useSelector } from 'react-redux'
+import Logout from '../../Logout'
 
 
 const NavMenu = () => {
   const [menuActive, setMenuActive] = useState(false)
+  const isAuthenticated = useSelector(state => !!state.auth.token)
 
   const cls = [
     classes.NavMenu_list,
     menuActive && classes.active
   ]
 
+  const clsUser = [
+    classes.list__item,
+    isAuthenticated ? classes.auth : null
+  ]
+
+  const renderLinks = (links) => {
+    return links.map((link, index) => {
+      return (
+        <li className={classes.list__item} key={index}>
+          <NavLink to={link.to}>{link.label}</NavLink>
+        </li>
+      )
+    })
+  }
+
   const onClose = () => setMenuActive(false)
-  
-  // const isSelected = (event) => {
-  //   const target = event.target.closest('.Menu_list__item__UbYEK')
-  // }
+
+  const links = [
+    {to:'/about', label: 'about'},
+    {to:'/search', label: 'search'}
+  ]
+
+  if (isAuthenticated) {
+    links.push({to:'/my_recipes', label: 'my recipes'})
+  }
 
   return (
     <Fragment>
       <nav className={classes.NavMenu}>
         <BurgerMenu menuActive={menuActive} setMenuActive={setMenuActive}/>
         <ul className={cls.join(' ')} onClick={onClose}>
-          <li className={classes.list__item}>
-            <NavLink to="/about"> about </NavLink>
-          </li>
-          <li className={classes.list__item}>
-            <NavLink to="/search"> search </NavLink>
-          </li>
-          <li className={classes.list__item}>
-            <NavLink to="/my_recipes"> my recipes </NavLink>
-          </li>
+          { renderLinks(links) }
         </ul>
         <div className={classes.NavMenu__icons}>
           <NavLink to="/">
             <img src={Logo} alt="Ri"/>
           </NavLink>
-          <NavLink to="/auth" className={classes.list__item}>
-            <img src={User} alt="Personal Area"/>
-          </NavLink>
+          { isAuthenticated
+            ? <Logout class={clsUser.join(' ')}/>
+            : <NavLink to="/auth" className={clsUser.join(' ')}>
+                <img src={User} alt="Login"/>
+              </NavLink>
+          }
         </div>
       </nav>
     </Fragment>
